a322e10cea0be47483ff614d90b4a591d43e070b
Nathalie Pires Cesarino <nathalie.cesarino@kroton.tech>
2021-12-19 20:14:30 -0300

    Initial Commit

---
+++ MemeMeApp1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMeApp1.0
+//
+//  Created by Nathalie Cesarino on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MemeMeApp1.0UITests/MemeMeApp1_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemeMeApp1_0UITestsLaunchTests.swift
+//  MemeMeApp1.0UITests
+//
+//  Created by Nathalie Cesarino on 19/12/21.
+//
+
+import XCTest
+
+class MemeMeApp1_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
